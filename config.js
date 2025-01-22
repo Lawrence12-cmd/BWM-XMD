a902f76da331f8f1df5bd3a34f88b393b9854621
Lawrence12-cmd <lawrenceyemoh2@gmail.com>
2025-01-22 17:10:12 +0000

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
-module.exports = { session: process.env.SESSION_ID || '',
+module.exports = { session: process.env.SESSION_ID || 'BWM-XMD;;;H4sIAAAAAAAAA5VU25KqOBT9l7ziOSBytaqrBhBRUURFW3pqaiqScFFuhiBil/8+Rdtd3Q9zzvTwlErI2itrrb1fQV4kFbZxC4avoCTJBVLcLWlbYjAEeh2GmIAeQJBCMATPy8It01qgEzQbO5ymmdZkdLwGR1JkqYq5dTSpdQbtRnv/Cdx7oKwPaRL8BjAmS2+W1raWZdauUNm139QqOnNHNEPl6Rwt2PFh6c9ItjWfwL1DhAlJ8sgsY5xhAlMbty5MyPfotxpBB5fXN5DfcyrcpUYwnTBscdXC+fx0sIwZEhfy2Na56nv00QqNt44a09W5UHVXNUvzme3rcIawUxoxNpesxKfCxuMXD/pVEuUYTRHOaULbb+u+cJhM2u6O4ybeNZOIJ3OHN/HLvM12HusvxqOjuZhl0vPeir5H3IcuRwXdi1bxdAmXc9PTr26dzVNpG2qsN1IiWzXUyF7uza/EXfKRldP/0X05fuFtNzJOsgAra0rWe8zxx11I/Ipng63jPxd2CDlhzk+/R1+arX324DJ4JO73rO6bmrGSNbYe1w5jtqlW9md4VbFrLRY+6UNak9+xpC8xubmFZi0VZTDho8iuxtfD3ubdiamwmIGzpmZO0rQ1FQauVy63WtX1uUz2rWSLEi+wFtlh63S+CqzorFNZL8NGi57eXnTC7RSBYf/eAwRHSUUJpEmRd3uS0AMQXTY4IJi+qQvULB2vy51a+ZocMN7a7A+CkJ0x0zPxXpaW2e7KsMrO5i2dPoEeKEkR4KrCaJJUtCDtAlcVjHAFhn/+1QM5vtKHb121Qb8HwoRUdJvXZVpA9GHqxyEMgqLO6abNA6NbYAKG3Oc2pjTJo6qTsc4hCeLkgo0Y0goMQ5hW+N4DCF+SAHd4QIvnL3xAQq+F3lzSdLPKr383HeW4yB+/KIMDluBh8ENAkvRDCFT5xyFQpB/9IJBEGPJKH0HQA8l7y3R3fungSts3Os5liI63SOXJch84Rptsx/7izYWH9JhgBIaU1LgHDjA41aVXnHD+G9zM8rbMRW/8uXy9TkeQD2FYHLE+j7kvuA9LwfD1c0wZBerwXHdvarKwAT2QvSUw6V7ODwbiQJBlSRWEIT/4o/rZdELCsvyZY9rhvove3UCYwiStwBAYTl6RF21iurl9nTeWpZmRZkQa+DTpI+yPNNmDeCIL01OJrYM1VflaOTbcgT3K1WS2y059tTQW4mgqOrHy9C8gYAiEm0C5MshmkZjfkHPaotTLRpDoWCqFeG2x5LL1jYWElM3RPq6ykvFWirOdJtfnWZCvORsuM0X0a3lsRCQwTjE+b0arp67aIyxfi0Uq0m9Csx3NlSYciGuRH9Rtf3RqlEtB0bFVDa7IT8q6Mq/jQtmvsmiv3EZzt9jp1saHh1ubsM7OT2QD3frmBKONq4yaRxu+jYH0ffwmbx3y+h6tMMFv0yyHnUX/Zc7XlHP33heM9/n4iyTpGyqsGcaY2fs4uyYuIpep4liswMh6u3spnRCujstVu/T8ENzvf/VAmUIaFiQDQwBzRIoEgR5IYUW1z0b1kgxXFGYlGPblgSxKoigLPZC1WlluKKQf/Q207pt7Crj/AywwWnX6BwAA',
     PREFIXE: process.env.PREFIX || ".",
     OWNER_NAME: process.env.OWNER_NAME || "Ibrahim Adams",
     NUMERO_OWNER : process.env.NUMERO_OWNER || " Ibrahim Adams",              
@@ -27,7 +27,7 @@ module.exports = { session: process.env.SESSION_ID || '',
     ANTIDELETE2 : process.env.ANTIDELETE2 || 'yes',
     MENUTYPE : process.env.MENUTYPE || '',
     ANTICALL : process.env.ANTICALL || 'yes',
-                  AUTO_REACT : process.env.AUTO_REACT || 'yes',
+                  AUTO_REACT : process.env.AUTO_REACT || 'no',
                   AUTO_REACT_STATUS : process.env.AUTO_REACT_STATUS || 'yes',
                   AUTO_REPLY : process.env.AUTO_REPLY || 'yes',
                   AUTO_READ : process.env.AUTO_READ || 'yes',
